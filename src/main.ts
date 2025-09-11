e84059c79d33df20363f7d608735d32eaebaa1b6
khoadd879 <khoadd879@gmail.com>
2025-09-11 21:17:47 +0700

    fix CORS

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT') || 3000;
   app.useGlobalPipes(
