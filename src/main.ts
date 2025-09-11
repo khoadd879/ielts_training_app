b347bf050314feb0e8641247999c62967f4fc407
khoadd879 <khoadd879@gmail.com>
2025-09-11 11:56:27 +0700

    add bearerAuth

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,7 @@ async function bootstrap() {
     .setTitle('Ielts Training App')
     .setDescription('The ielts training app API description')
     .setVersion('1.0')
+    .addBearerAuth()
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
