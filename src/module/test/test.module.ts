1ea1fc47664103683410531fa746fd82d0006afe
khoadd879 <khoadd879@gmail.com>
2025-09-25 12:34:56 +0700

    fix bug

--- src/module/test/test.module.ts
+++ src/module/test/test.module.ts
@@ -4,9 +4,18 @@ import { TestController } from './test.controller';
 import { DatabaseModule } from 'src/database/database.module';
 import { UsersModule } from '../users/users.module';
 import { CloudinaryModule } from 'src/cloudinary/cloudinary.module';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
-  imports: [DatabaseModule, UsersModule, CloudinaryModule],
+  imports: [
+    DatabaseModule,
+    UsersModule,
+    CloudinaryModule,
+    CacheModule.register({
+      ttl: 5, // time-to-live (seconds)
+      max: 100, // maximum number of items in cache
+    }),
+  ],
   controllers: [TestController],
   providers: [TestService],
 })
