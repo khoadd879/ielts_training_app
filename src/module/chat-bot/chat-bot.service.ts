2d06c01d76e94cb3e074868b5897ff92dfc610d6
khoadd879 <khoadd879@gmail.com>
2025-10-13 21:47:35 +0700

    fix more

--- src/module/chat-bot/chat-bot.service.ts
+++ src/module/chat-bot/chat-bot.service.ts
@@ -12,15 +12,16 @@ export class ChatBotService {
 
   async saveMessage(idUser: string, message: string) {
     const key = this.getUserKey(idUser);
-    const oldMessages = ((await this.cacheManager.get<string[]>(key)) ||
-      []) as string[];
+    const oldRaw = await this.cacheManager.get<string>(key);
+    const oldMessages = oldRaw ? JSON.parse(oldRaw) : [];
     const newMessages = [...oldMessages, message];
-    await this.cacheManager.set(key, newMessages);
+    await this.cacheManager.set(key, JSON.stringify(newMessages));
   }
 
   async getMessages(idUser: string): Promise<string[]> {
     const key = this.getUserKey(idUser);
-    return ((await this.cacheManager.get<string[]>(key)) || []) as string[];
+    const raw = await this.cacheManager.get<string>(key);
+    return raw ? JSON.parse(raw) : [];
   }
 
   async clearMessages(idUser: string) {
