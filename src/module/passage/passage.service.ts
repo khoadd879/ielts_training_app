f76b1cdd37f4850e63c65d5f75684e8365449d1b
khoadd879 <khoadd879@gmail.com>
2025-10-30 23:20:21 +0700

    fix bug

--- src/module/passage/passage.service.ts
+++ src/module/passage/passage.service.ts
@@ -117,6 +117,14 @@ export class PassageService {
       image = uploadResult.secure_url;
     }
 
+    const numParagraph =
+      typeof numberParagraph === 'number'
+        ? numberParagraph
+        : Number.parseInt(String(numberParagraph), 10);
+
+    if (Number.isNaN(numParagraph))
+      throw new BadRequestException('numberParagraph must be an integer');
+
     const data = await this.databaseService.doanVan.update({
       where: {
         idDoanVan: idPassage,
@@ -127,7 +135,7 @@ export class PassageService {
         content,
         image,
         description,
-        numberParagraph,
+        numberParagraph: numParagraph,
       },
     });
     return {
