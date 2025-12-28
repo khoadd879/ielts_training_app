12d6616e544c9846256ef75a596fa49ba0a2ccb6
khoa <khoadd879@gmail.com>
2025-12-28 17:49:08 +0700

    add property api

--- src/module/speaking-task/dto/create-speaking-task.dto.ts
+++ src/module/speaking-task/dto/create-speaking-task.dto.ts
@@ -1,13 +1,17 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { SpeakingPartType } from '@prisma/client';
 import { IsEnum, IsNotEmpty } from 'class-validator';
 
 export class CreateSpeakingTaskDto {
+  @ApiProperty({example: '123'})
   @IsNotEmpty()
   idTest: string;
 
+  @ApiProperty({example: 'title'})
   @IsNotEmpty()
   title: string;
 
+  @ApiProperty({example: 'PART1'})
   @IsNotEmpty()
   @IsEnum(SpeakingPartType)
   part: SpeakingPartType
