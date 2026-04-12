6cbc51a54c26cd47699a8997ac21336e40d7664b
KhangKhangKhangKhang <duongtrongkhang121005@gmail.com>
2026-04-12 16:39:47 +0700

    feat: teacher dashboard

--- src/app.module.ts
+++ src/app.module.ts
@@ -39,6 +39,7 @@ import { RecommendTestModule } from './module/recommend-test/recommend-test.modu
 import { SpeakingTaskModule } from './module/speaking-task/speaking-task.module';
 import { SpeakingQuestionModule } from './module/speaking-question/speaking-question.module';
 import { UserSpeakingSubmissionModule } from './module/user-speaking-submission/user-speaking-submission.module';
+import { DashboardModule } from './module/dashboard/dashboard.module';
 
 import * as redisStore from 'cache-manager-redis-store';
 import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
@@ -115,6 +116,8 @@ import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
     SpeakingQuestionModule,
 
     UserSpeakingSubmissionModule,
+
+    DashboardModule,
   ],
   controllers: [AppController],
   providers: [
